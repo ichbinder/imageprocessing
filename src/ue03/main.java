62866efbd60ff63274465991043a134216065115
Jakob Warnow <jakob.warnow@gmx.de>
2015-11-18 13:53:14 +0100

    jakob: ja versuche es aber ist nicht so weinfach ab hier neu !

--- src/ue03/main.java
+++ src/ue03/main.java
@@ -1,143 +1,184 @@
 package ue03;
 
-import java.util.HashSet;
-import java.util.Set;
-import java.awt.Point;
+import java.awt.image.BufferedImage;
+import java.awt.image.PixelGrabber;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
 
 public class main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException, IOException {
 		// TODO Auto-generated method stub
-		
-		int [] array = new int []{0,1,1,1};
-		
-		double arrowDirection = 270;
-		Point [] pixelCoordinates = new Point[] { new Point(1,2), new Point(1,3), new Point(2,2), new Point(2,3)};
-		
-		System.out.println(array[0] + "|" +array[1]);
-		System.out.println(array[2] + "|" +array[3]);
-		
-		main m = new main();
-/*		
-		int [] rotate = m.rotate(90, array);
-		System.out.println("----90");
-		System.out.println(rotate[0] + "|" +rotate[1]);
-		System.out.println(rotate[2] + "|" +rotate[3]);
-		*/
-		System.out.println("----180");
-		
-		int [] rotate2 = m.rotate(180, array);
-		
-		System.out.println(rotate2[0] + "|" +rotate2[1]);
-		System.out.println(rotate2[2] + "|" +rotate2[3]);
-		
-		System.out.println("FORWARD");
-		
-		
-/*		
-		System.out.println("----270");
-		
-		int [] rotate3 = m.rotate(270, array);
-		
-		System.out.println(rotate3[0] + "|" +rotate3[1]);
-		System.out.println(rotate3[2] + "|" +rotate3[3]);
-*/				
-	}
-	
+		File imgFile = new File("/home/jakob/git/imageprocessing/klein.png");
+	    BufferedImage hugeImage = ImageIO.read(imgFile);
+		PixelGrabber pg = new PixelGrabber(hugeImage, 0, 0, hugeImage.getWidth(), hugeImage.getHeight(), false);
 
-	/**Bildet ein 1-dimensionales in ein 2-dimensionales Pixel-Array
-	 * @param src Pixeldaten (1 dim)
-	 * @param width Bildbreite
-	 * @param height Bildhöhe
-	 * @return 2-dimensionales Pixel-Array*/
-	private int [][] prepareBinaryImage(int [] src, int width, int height){
-		
-		int [][] pixels = new int [height][width];
+		pg.grabPixels(); // Throws InterruptedException
+
+		int[] pixels = (int[])pg.getPixels();
+		main main = new main();
+		main.RegionLabeling(pixels, pg.getWidth(), pg.getHeight());
+	}
+
+	/**
+	 * Bildet ein 1-dimensionales in ein 2-dimensionales Pixel-Array
+	 * 
+	 * @param src
+	 *            Pixeldaten (1 dim)
+	 * @param width
+	 *            Bildbreite
+	 * @param height
+	 *            Bildhöhe
+	 * @return 2-dimensionales Pixel-Array
+	 */
+	private int[][] prepareBinaryImage(int[] src, int width, int height) {
+
+		int[][] pixels = new int[height][width];
 		int i = 0;
-		
-		for(int h = 0; h < height; h++){
-			
-			for(int w = 0; w < width; w++){
-				
+
+		for (int h = 0; h < height; h++) {
+
+			for (int w = 0; w < width; w++) {
+
 				pixels[h][w] = src[i];
 				i++;
 			}
-		}		
+		}
 		return pixels;
 	}
-	
-	public void RegionLabeling(int [] input, int width, int height) {
-		int m = 2;
+
+	public void RegionLabeling(int[] input, int width, int height) {
 
 		int[][] pixels = prepareBinaryImage(input, width, height);
 
 		long timeStart = System.nanoTime();
-		
+
 		for (int h = 0; h < height; h++) {
 			for (int w = 0; w < width; w++) {
 				if ((pixels[h][w] & 0x00ffffff) == 0) {
-//					pixels = this.BreadthFirst8Without(pixels, h, w, m);
-					m = m + 1;
-//					System.out.println(pixels[h][w]);
+					potrace(pixels, h, w);
 				}
 			}
 		}
-		
-		int outputPixels[] = new int [width * height];
+
+		int outputPixels[] = new int[width * height];
 		int i = 0;
-		for(int h = 0; h < height; h++){
-			for(int w = 0; w < width; w++){
-				
-				
+		for (int h = 0; h < height; h++) {
+			for (int w = 0; w < width; w++) {
+
 				outputPixels[i] = pixels[h][w];
 				i++;
 			}
 		}
 		System.arraycopy(outputPixels, 0, input, 0, input.length);
-		
+
 		long timeStop = System.nanoTime();
-		
+
 		System.out.println("Calc Time: " + (timeStop - timeStart));
-//		System.out.println("Stack max length: " + lengthOfS);		
 	}
-	
-	public void potrace(int[][] pixels, int height, int width) {
-		int[] start = {height, width};
-		Set edge = new HashSet<>();
-		int[] end = {};
-		
-		while (!(start==end)) {
-			if (end[0] > -1 && end[0] < pixels[0].length && end[1] > -1 && end[1] < pixels[1].length
-					&& (pixels[end[0]][end[1]] & 0x00ffffff) == 0) {	
+
+	public void potrace(int[][] pixels, int x, int y) {
+		int[] start = { x, y };
+		int[] end = {0, 0};
+		int arrowDirection = 0;
+		int[] patern = {0, 0, 0, 0};
+
+		while (!(start == end)) {
+			end[0] = x;
+			end[1] = y;
+//			v
 				
-			}
+				while (arrowDirection != 360) {
+					if (arrowDirection == 0) {
+						System.out.println(pixels[end[0]][end[1]] + " " + end[0] + ":" + end[1]);
+						System.out.println(pixels[end[0]][end[1] - 1] + " " + (end[0]) + ":" + (end[1] - 1));
+						System.out.println(pixels[end[0] + 1][end[1]] + " " + (end[0] + 1) + ":" + (end[1]));
+						System.out.println(pixels[end[0] + 1][end[1] - 1] + " " + (end[0] + 1) + ":" + (end[1] - 1));
+						
+						patern[1] = pixels[end[0]][end[1] - 1] == -1 ? 0 : 1;
+						patern[2] = pixels[end[0] + 1][end[1]] == -1 ? 0 : 1;
+						patern[3] = pixels[end[0] + 1][end[1] - 1] == -1 ? 0 : 1;
+						patern = rotate(180, patern);
+						checkPatern(patern);
+					} else if (arrowDirection == 90) {
+						System.out.println(pixels[end[0]][end[1]] + " " + end[0] + ":" + end[1]);
+						System.out.println(pixels[end[0] + 1][end[1]] + " " + (end[0] + 1) + ":" + (end[1]));
+						System.out.println(pixels[end[0]][end[1] + 1] + " " + (end[0]) + ":" + (end[1] + 1));
+						System.out.println(pixels[end[0] + 1][end[1] + 1] + " " + (end[0] + 1) + ":" + (end[1] + 1));
+						
+						patern[1] = pixels[end[0] + 1][end[1]] == -1 ? 0 : 1;
+						patern[2] = pixels[end[0]][end[1] + 1] == -1 ? 0 : 1;
+						patern[3] = pixels[end[0] + 1][end[1] + 1] == -1 ? 0 : 1;
+						patern = rotate(90, patern);
+						checkPatern(patern);
+					} else if (arrowDirection == 180) {
+						System.out.println(pixels[end[0]][end[1]] + " " + end[0] + ":" + end[1]);
+						System.out.println(pixels[end[0]][end[1] + 1] + " " + (end[0]) + ":" + (end[1] + 1));
+						System.out.println(pixels[end[0] - 1][end[1]] + " " + (end[0] - 1) + ":" + (end[1]));
+						System.out.println(pixels[end[0] - 1][end[1] + 1] + " " + (end[0] - 1) + ":" + (end[1] + 1));
+						
+						patern[1] = pixels[end[0]][end[1] + 1] == -1 ? 0 : 1;
+						patern[2] = pixels[end[0] - 1][end[1]] == -1 ? 0 : 1;
+						patern[3] = pixels[end[0] - 1][end[1] + 1] == -1 ? 0 : 1;
+						checkPatern(patern);
+					} else if (arrowDirection == 270) {
+						System.out.println(pixels[end[0]][end[1]] + " " + end[0] + ":" + end[1]);
+						System.out.println(pixels[end[0] - 1][end[1]] + " " + (end[0] - 1) + ":" + (end[1]));
+						System.out.println(pixels[end[0]][end[1] - 1] + " " + (end[0]) + ":" + (end[1] - 1));
+						System.out.println(pixels[end[0] - 1][end[1] - 1] + " " + (end[0] - 1) + ":" + (end[1] - 1));
+						
+						patern[1] = pixels[end[0] - 1][end[1]] == -1 ? 0 : 1;
+						patern[2] = pixels[end[0]][end[1] - 1] == -1 ? 0 : 1;
+						patern[3] = pixels[end[0] - 1][end[1] - 1] == -1 ? 0 : 1;
+						patern = rotate(270, patern);
+						checkPatern(patern);
+					}
+					arrowDirection = arrowDirection + 90; 
+					
+				}
+//			}
 		}
 	}
-	
-	private int [] rotate(double degree, int [] input){
-		
-		int [] rotatedArray = new int [4];
-		
-//		System.arraycopy(input, 0, rotatedArray, 0, rotatedArray.length);
-		
-		if(degree == 90){
+
+	/*
+	 * Patern id:
+	 * 
+	 * |2|3| 
+	 * |0|1|
+	 */
+
+	private void checkPatern(int[] patern) {
+		if (patern.length != 4) 
+			throw new IllegalArgumentException();
+		if (patern[0] == 1 & patern[1] == 1 & patern[2] == 0 & patern[3] == 1) {
+			
+		} else if (patern[0] == 1 && patern[1] == 0 && patern[2] == 0 && patern[3] == 1) {
+			
+		} else if (patern[0] == 1 && patern[1] == 0 && patern[2] == 0 && patern[3] == 1) {
+			
+		} else if (patern[0] == 1 && patern[1] == 0 && patern[2] == 0 && patern[3] == 1) {
+			
+		}
+	}
+
+	private int[] rotate(double degree, int[] input) {
+
+		int[] rotatedArray = new int[4];
+
+		if (degree == 90) {
 			rotatedArray[0] = input[1];
 			rotatedArray[1] = input[3];
 			rotatedArray[2] = input[0];
-			rotatedArray[3] = input[2];			
-		}
-		
-		else if (degree == 180){
-			
-			rotatedArray = rotate(90, input);			
+			rotatedArray[3] = input[2];
+		} else if (degree == 180) {
+			rotatedArray = rotate(90, input);
+			rotatedArray = rotate(90, rotatedArray);
+		} else if (degree == 270) {
+			rotatedArray = rotate(180, input);
 			rotatedArray = rotate(90, rotatedArray);
 		}
-		else if (degree == 270){
-			
-			rotatedArray = rotate(180, input);			
-			rotatedArray = rotate(90, rotatedArray);			
-		}
-		
+
 		return rotatedArray;
 	}
 
