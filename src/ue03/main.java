034ed8306f23d9df2f2934c9067e69038b158f14
andrevallentin <andrevallentin@192.168.2.111>
2015-11-18 19:43:50 +0100

    pattern als 2D-Array und rotation auch mit 2D möglich.

--- src/ue03/main.java
+++ src/ue03/main.java
@@ -4,7 +4,12 @@ import java.awt.image.BufferedImage;
 import java.awt.image.PixelGrabber;
 import java.io.File;
 import java.io.IOException;
+import java.util.LinkedList;
+import java.util.Queue;
+
 import javax.imageio.ImageIO;
+
+import com.sun.javafx.geom.Vec2d;
 
 public class main {
 
@@ -81,11 +86,32 @@ public class main {
 		System.out.println("Calc Time: " + (timeStop - timeStart));
 	}
 
+	
+	private int convertToLabeling(int value){
+		
+		return value == -1 ? 0 : 1; 
+	}
+	private void convertPattern(int [][] pattern){
+		
+		for(int i = 0; i < 2; i++){			
+			for(int j = 0; j < 2; j++) pattern[i][j] = convertToLabeling(pattern[i][j]);
+		}
+	}
+	
 	public void potrace(int[][] pixels, int x, int y) {
+
+		Queue vecQueue = new LinkedList<Vec2d>();
+		Vec2d vg2d = new Vec2d();		
 		int[] start = { x, y };
 		int[] end = {0, 0};
 		int arrowDirection = 0;
-		int[] patern = {0, 0, 0, 0};
+		int[] patern = {0, 0, 0, 0};		
+		
+		int[][] pattern2D = new int [2][2];
+		
+		vg2d.set(x, y);
+		
+		vecQueue.add(vg2d);
 
 		while (!(start == end)) {
 			end[0] = x;
@@ -94,6 +120,15 @@ public class main {
 				
 				while (arrowDirection != 360) {
 					if (arrowDirection == 0) {
+						
+						pattern2D[0][0] = pixels[y][x];
+						pattern2D[0][1] = pixels[y][x+1];
+						pattern2D[1][0] = pixels[y+1][x];
+						pattern2D[1][1] = pixels[y+1][x+1];
+
+						//Convert to readable numbers
+						convertPattern(pattern2D);
+						
 						System.out.println(pixels[end[0]][end[1]] + " " + end[0] + ":" + end[1]);
 						System.out.println(pixels[end[0]][end[1] - 1] + " " + (end[0]) + ":" + (end[1] - 1));
 						System.out.println(pixels[end[0] + 1][end[1]] + " " + (end[0] + 1) + ":" + (end[1]));
@@ -102,8 +137,13 @@ public class main {
 						patern[1] = pixels[end[0]][end[1] - 1] == -1 ? 0 : 1;
 						patern[2] = pixels[end[0] + 1][end[1]] == -1 ? 0 : 1;
 						patern[3] = pixels[end[0] + 1][end[1] - 1] == -1 ? 0 : 1;
-						patern = rotate(180, patern);
-						checkPatern(patern);
+												
+//						patern = rotate(180, patern);
+						checkPattern(patern, 180);
+						
+						
+						
+						
 					} else if (arrowDirection == 90) {
 						System.out.println(pixels[end[0]][end[1]] + " " + end[0] + ":" + end[1]);
 						System.out.println(pixels[end[0] + 1][end[1]] + " " + (end[0] + 1) + ":" + (end[1]));
@@ -113,8 +153,9 @@ public class main {
 						patern[1] = pixels[end[0] + 1][end[1]] == -1 ? 0 : 1;
 						patern[2] = pixels[end[0]][end[1] + 1] == -1 ? 0 : 1;
 						patern[3] = pixels[end[0] + 1][end[1] + 1] == -1 ? 0 : 1;
-						patern = rotate(90, patern);
-						checkPatern(patern);
+//						patern = rotate(90, patern);
+						checkPattern(patern, 90);
+												
 					} else if (arrowDirection == 180) {
 						System.out.println(pixels[end[0]][end[1]] + " " + end[0] + ":" + end[1]);
 						System.out.println(pixels[end[0]][end[1] + 1] + " " + (end[0]) + ":" + (end[1] + 1));
@@ -124,7 +165,9 @@ public class main {
 						patern[1] = pixels[end[0]][end[1] + 1] == -1 ? 0 : 1;
 						patern[2] = pixels[end[0] - 1][end[1]] == -1 ? 0 : 1;
 						patern[3] = pixels[end[0] - 1][end[1] + 1] == -1 ? 0 : 1;
-						checkPatern(patern);
+						
+						checkPattern(patern, 0);
+						
 					} else if (arrowDirection == 270) {
 						System.out.println(pixels[end[0]][end[1]] + " " + end[0] + ":" + end[1]);
 						System.out.println(pixels[end[0] - 1][end[1]] + " " + (end[0] - 1) + ":" + (end[1]));
@@ -134,8 +177,9 @@ public class main {
 						patern[1] = pixels[end[0] - 1][end[1]] == -1 ? 0 : 1;
 						patern[2] = pixels[end[0]][end[1] - 1] == -1 ? 0 : 1;
 						patern[3] = pixels[end[0] - 1][end[1] - 1] == -1 ? 0 : 1;
-						patern = rotate(270, patern);
-						checkPatern(patern);
+						
+//						patern = rotate(270, patern);
+						checkPattern(patern,270);
 					}
 					arrowDirection = arrowDirection + 90; 
 					
@@ -150,23 +194,59 @@ public class main {
 	 * |2|3| 
 	 * |0|1|
 	 */
-
-	private void checkPatern(int[] patern) {
-		if (patern.length != 4) 
+	
+	
+	private void checkPattern(int[][] pattern, int rotate) {
+		
+		//Rotiere hier erst das Pattern
+		pattern = rotate(rotate, pattern);
+		
+		if (pattern.length != 4) 
 			throw new IllegalArgumentException();
 		
-		//Rechts abbiegen
-		if (patern[0] == 0 & patern[1] == 0 & patern[2] == 0 & patern[3] == 1) {
-			patern[1] = 2;
+		//Rechts abbiegen // -> 
+		if (pattern[0][0] == 0 & pattern[0][1] == 0 & pattern[1][0] == 0 & pattern[1][1] == 1) {
+			pattern[0][1] = 2;
 		} 
-		//Gerade aus
-		else if (patern[0] == 0 && patern[1] == 1 && patern[2] == 0 && patern[3] == 1) {
-			
-		} else if (patern[0] == 1 && patern[1] == 0 && patern[2] == 0 && patern[3] == 1) {
-			
-		} else if (patern[0] == 1 && patern[1] == 0 && patern[2] == 0 && patern[3] == 1) {
+		//Geradeaus
+		else if (pattern[0][0] == 0 && pattern[0][1] == 1 && pattern[1][0] == 0 && pattern[1][1] == 1) {
+			pattern[0][0] = 2;			
+		}
+		//Links abbiegen
+		else if (pattern[0][0] == 1 && pattern[0][1] == 1 && pattern[1][0] == 0 && pattern[1][1] == 1) {
+		
+		} 		
+		else if (pattern[0][0] == 1 && pattern[0][1] == 1 && pattern[1][0] == 0 && pattern[1][1] == 1) {
 			
 		}
+		//Rotiere das Pattern zurück		
+		pattern = rotate(360 - rotate, pattern);
+	}
+
+
+	private void checkPattern(int[] pattern, int rotate) {
+		
+		//Rotiere hier erst das Pattern
+		pattern = rotate(rotate, pattern);
+		
+		if (pattern.length != 4) 
+			throw new IllegalArgumentException();
+		
+		//Rechts abbiegen // -> 
+		if (pattern[0] == 0 & pattern[1] == 0 & pattern[2] == 0 & pattern[3] == 1) {			
+			pattern[1] = 2;
+		} 
+		//Geradeaus
+		else if (pattern[0] == 0 && pattern[1] == 1 && pattern[2] == 0 && pattern[3] == 1) {
+			pattern[0] = 2;
+			
+		}//Links abbiegen
+		else if (pattern[0] == 1 && pattern[1] == 1 && pattern[2] == 0 && pattern[3] == 1) {
+		} else if (pattern[0] == 1 && pattern[1] == 0 && pattern[2] == 0 && pattern[3] == 1) {
+			
+		}
+		//Rotiere das Pattern zurück		
+		pattern = rotate(360 - rotate, pattern);
 	}
 
 	private int[] rotate(double degree, int[] input) {
@@ -188,5 +268,26 @@ public class main {
 
 		return rotatedArray;
 	}
+	
+	private int[][] rotate(double degree, int[][] input) {
+
+		int[][] rotatedArray = new int[2][2];
+
+		if (degree == 90) {
+			rotatedArray[0][0] = input[0][1];
+			rotatedArray[0][1] = input[1][2];
+			rotatedArray[1][0] = input[0][0];
+			rotatedArray[1][1] = input[1][0];
+		} else if (degree == 180) {
+			rotatedArray = rotate(90, input);
+			rotatedArray = rotate(90, rotatedArray);
+		} else if (degree == 270) {
+			rotatedArray = rotate(180, input);
+			rotatedArray = rotate(90, rotatedArray);
+		}
+
+		return rotatedArray;
+	}
+
 
 }
